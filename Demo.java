17a41e28e834c7b5fb68dcc985a56527fffa574a
zhuyuandan <zyd130150@163.com>
2018-01-01 19:20:14 +0800

    Change Two Files Print Content

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Hello{
 
 	public static void main(String args[]){
-		System.out.println("HELLO yootk.");
+		System.out.println("Hello World .");
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 
 	public static void main(String args[]){
-		System.out.println("www.mldn.com");
+		System.out.println("www.yootk.com");
 	}
 }
