af4c58227c07a86ed315f728ba6a6fc10f615b91
zhuyuandan <zyd130150@163.com>
2018-01-01 22:46:14 +0800

    add static attribute

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
-
+	public static final String COMPANY_NAME="yootk";
 	public static void main(String args[]){
 		System.out.println("Happy New Year");
 		System.out.println("Happy MySelf");
