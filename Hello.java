b65e4e4b54adebf2f0902f7c60d4750b2aea722c
zhuyuandan <zyd130150@163.com>
2018-01-01 22:53:25 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,13 +1,6 @@
 public class Hello{
-	public static final String COMPANY_NAME="yootk";
 	public static void main(String args[]){
-		System.out.println("Happy New Year");
-		System.out.println("Happy MySelf");
-		print();
-	}
-	public static void print(){
-		System.out.println("**********************");
-		System.out.println("*     Hello World!   *");
-		System.out.println("**********************");
+		System.out.println("********");
+		System.out.println("########");
 	}
 }
