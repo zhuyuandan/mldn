ff21913dc4b1b77e57ef43ecc99fe463adcb1eb1
zhuyuandan <zyd130150@163.com>
2018-01-01 17:39:30 +0800

    Update Hello.java File . Add Two Lines

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 
 	public static void main(String args[]){
-		System.out.println("Hello World!");
+		System.out.println("Hello Yootk.");
+		System.out.println("www.yootk.com");
 	}
 }
